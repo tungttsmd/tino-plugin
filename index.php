84bf8aef1767a3a24eec4e875b30337325474173
thanhtung.tran2k <thanhtung.tran2k@gmail.com>
2025-05-05 23:20:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,6 @@ if (!defined('ABSPATH')) {
     exit; // Ngăn truy cập trực tiếp
 }
 
-
 add_action('init', function () {
     require_once plugin_dir_path(__FILE__) . 'plugin_function/function.php';
 });
